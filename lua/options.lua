ba58f874b2d8ba2b53f9966e101d376798d6837c
arsen <baregamyan@gmail.com>
2024-05-17 15:55:33 +0300

    add ejs support

--- lua/options.lua
+++ lua/options.lua
@@ -6,5 +6,11 @@ vim.cmd([[
   let g:sneak#use_ic_scs = 1
   let g:sneak#absolute_dir = 1
 ]])
+
+vim.filetype.add({
+  extension = {
+    ejs = "html",
+  }
+})
 -- local o = vim.o
 -- o.cursorlineopt ='both' -- to enable cursorline!
